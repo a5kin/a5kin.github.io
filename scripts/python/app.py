ddf0882cce117f2869511b77619dbdaedfeab557
Andrey Zamaraev <a5kin@users.noreply.github.com>
2021-03-17 03:09:53 +0300

    Add Brython/Pixi experiment

---
+++ scripts/python/app.py
@@ -0,0 +1,76 @@
+from browser import document, window
+import math
+import random
+
+NUM_CIRCLES = 100
+
+
+class ExcentricCircle:
+    """Excentric circle sumulation"""
+
+    def __init__(self, x_e, y_e, r_e, r_c, fi, dfi, color, opacity):
+        self.x_e, self.y_e = x_e, y_e
+        self.r_e = r_e
+        self.fi, self.dfi = fi, dfi
+        self.color = color
+        self.opacity = opacity
+        self.x, self.y, self.r = 0, 0, r_c
+        self.update()
+
+    def update(self):
+        """Move and update circle position."""
+        self.fi += self.dfi
+        self.x = -self.r_e * math.sin(self.fi) + self.x_e
+        self.y = self.r_e * math.cos(self.fi) + self.y_e
+
+
+class DevApp:
+    """Main app for dev page."""
+
+    def __init__(self):
+        """Initialize the app engine."""
+        # setup Pixi
+        self.app = window.PIXI.Application.new({"width": 256, "height": 256})
+        document.body.appendChild(self.app.view)
+        self.app.renderer.view.style.position = "absolute"
+        self.app.renderer.view.style.display = "block"
+        self.app.renderer.autoResize = True
+        self.app.renderer.resize(window.innerWidth, window.innerHeight)
+
+        # add graphics context for drawing
+        self.ctx = window.PIXI.Graphics.new()
+        self.ctx.x = window.innerWidth / 2
+        self.ctx.y = window.innerHeight / 2
+        self.app.stage.addChild(self.ctx)
+
+        self.excentrics = []
+        self.create_excentrics()
+
+    def create_excentrics(self):
+        """Create excentric circles for the stage."""
+        for _ in range(NUM_CIRCLES):
+            x_e = random.randint(-100, 100)
+            y_e = random.randint(-100, 100)
+            r_e = 42 * (1 + random.random())
+            r_c = 69 * (1 + random.random())
+            fi = 2 * math.pi * random.random()
+            dfi = (0.5 - random.random()) * math.pi / 50
+            color = 0x9966FF #* random.randint(0, 1)
+            opacity = 1 / 255
+            circle = ExcentricCircle(x_e, y_e, r_e, r_c,
+                                     fi, dfi, color, opacity)
+            self.excentrics.append(circle)
+
+    def draw(self, *args):
+        """Redraw the whole app."""
+        self.ctx.clear()
+        for circle in self.excentrics:
+            self.ctx.beginFill(circle.color, circle.opacity)
+            self.ctx.drawCircle(circle.x, circle.y, circle.r)
+            self.ctx.endFill()
+            circle.update()
+        window.requestAnimationFrame(self.draw)
+
+
+main_app = DevApp()
+main_app.draw()
